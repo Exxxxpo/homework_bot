dfd555513b86900bf6bc6603836b26294ffccb80
lev <ldevaev@mail.ru>
2022-11-25 14:43:12 +0500

    test

--- homework.py
+++ homework.py
@@ -1,10 +1,11 @@
 import os
 import sys
 import time
+import logging
 
 import requests
 from dotenv import load_dotenv
-from telegram import Bot
+import telegram
 
 load_dotenv()
 
@@ -21,17 +22,26 @@ HOMEWORK_VERDICTS = {
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
 def check_tokens():
     """Проверка доступности переменных окружения"""
     if not PRACTICUM_TOKEN and not TELEGRAM_TOKEN:
+        logging.critical('Проверьте переменные окружения')
         sys.exit()
 
 
 def send_message(bot, message):
     """Отправка сообщения пользователю"""
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logging.debug('Сообщение отправлено ботом!')
+    except Exception as error:
+        logging.error(error, 'Сообщение не отправилось ботом')
 
 
 def get_api_answer(timestamp):
@@ -40,33 +50,44 @@ def get_api_answer(timestamp):
         response = requests.get(url=ENDPOINT, headers=HEADERS,
                                 params={'from_date': timestamp})
         if response.status_code != 200:
+            logging.error('Status code error')
             raise Exception
     except requests.exceptions.RequestException:
+        logging.error('Сбой при запросе к эндпоинту')
+        raise Exception
         SystemExit('Something wrong')
     # except requests.exceptions.ConnectionError as errc:
     #     print ("Error Connecting:", errc)
-    print(response.json())
+    # print(response.json())
     return response.json()
 
 
 def check_response(response):
     """Проверка API на соответствие документации"""
     if type(response) != dict:
+        logging.error('API возвращает не словарь')
         raise TypeError
     if 'homeworks' not in response:
+        logging.error('нет ключа "homeworks"')
         raise KeyError
     if type(response['homeworks']) != list:
+        logging.error('response["homeworks"] возвращает не список')
         raise TypeError
+    if not response.get('homeworks'):
+        logging.error('response["homeworks"] не имеет значения')
+        raise Exception
 def parse_status(homework):
     """Извлекает из информации о конкретной домашней работе статус работы"""
     try:
         homework_name = homework.get('homework_name')
         if not homework_name:
+            logging.error('Нет ключа homework_name')
             raise KeyError
         verdict = HOMEWORK_VERDICTS[homework['status']]
         # if not homework.get('status'):
         #     raise KeyError
     except KeyError:
+        logging.error('Wrong key')
         raise Exception
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -74,7 +95,7 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
     check_tokens()
-    bot = Bot(token=TELEGRAM_TOKEN)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())  # int(time.time())
     cache_response = ''
     while True:
@@ -87,10 +108,11 @@ def main():
                 print(message)  # delete
                 send_message(bot, message)
                 cache_response = response
-            time.sleep(5)  # set RETRY_PERIOD here
+            time.sleep(10)  # set RETRY_PERIOD here
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
+            print(message)
 
 
 if __name__ == '__main__':
